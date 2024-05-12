7e1cc52dc2c5cd2e276fd93c96608e07a76100c9
Nayim González Rodríguez <60991432+nayimgr@users.noreply.github.com>
2024-05-12 18:16:43 +0200

    Functional script

--- pae_plotter.py
+++ pae_plotter.py
@@ -5,11 +5,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import glob
+import string
+import argparse
 
-
-# Replace 'file_path.json' with the actual path to your JSON file
-directory_path = '.'
-# file_path = 'fold_cst_telojunction_job_request.json'
+# Parse input
+parser = argparse.ArgumentParser(description='Script for plotting pae from all models from AF3 server')
+parser.add_argument('--dir', type=str, help='directory containing all json files', default="." )
+args = parser.parse_args()
+directory_path = args.dir
 
 def get_full_data(path):
     job_parameters_file = glob.glob(path+"/*job_request.json")[0]
@@ -17,66 +20,61 @@ def get_full_data(path):
         parameters = json.load(p)[0]
     name = parameters['name']
     sequences = parameters['sequences']
-    print(sequences[0]['proteinChain'])
     protein_lengths = []
     for i in sequences:
-        try: protein_lengths.append(len(i['proteinChain']['sequence'])*i['proteinChain']['count'])
-        except:
-            try: protein_lengths.append(len(i['dnaChain']['sequence'])*i['dnaChain']['count'])
-            except: 
-                try: protein_lengths.append(len(i['ion']['sequence'])*i['ion']['count'])
-                except:
-                    try: protein_lengths.append(len(i['rnaChain']['sequence'])*i['rnaChain']['count'])
-                    except:
-                        print("wtf")
-                        exit()
+        if "proteinChain" in i:
+            for n in range(int(i['proteinChain']['count'])):
+                protein_lengths.append(len(i['proteinChain']['sequence']))
+        elif "dnaChain" in i:
+            for n in range(int(i['dnaChain']['count'])):
+                protein_lengths.append(len(i['dnaChain']['sequence']))
+        elif "rnaChain" in i:
+            for n in range(int(i['rnaChain']['count'])):
+                protein_lengths.append(len(i['rnaChain']['sequence']))
+        elif "ion" in i:
+            for n in range(int(i['ion']['count'])):
+                protein_lengths.append(len(i['ion']['sequence']))
+        elif "ligand" in i:
+            for n in range(int(i['ligand']['count'])):
+                protein_lengths.append(len(i['ligand']['sequence']))
+        else: print("There is a molecule I cannot recognize in "+str(i))
     file_list = sorted(glob.glob(path+"/*full_data_?.json"))
     all_preds = []
     for file in file_list:
         with open(file, 'r') as f:
             all_preds.append(json.load(f))
-    return [all_preds,protein_lengths]
+    return [all_preds,protein_lengths,name]
 
-all_data = get_full_data(directory_path)[0]
-proteins_lengths =  get_full_data(directory_path)[1]
-total_length = sum(proteins_lengths)
-
-name = "pae_"
-n_model = "0"
-num_models = len(all_data)
-fig, axes = plt.subplots(1, num_models+1, figsize=((8*num_models)+1, 6))
-
-color_bar_bool = False
-for i, model in enumerate(all_data):
-    pae_matrix = np.array(model['pae'])
-    pred_len = pae_matrix.shape[0]
-    if i == len(all_data)-1: color_bar_bool = True
-    sns.heatmap(pae_matrix, cmap='viridis', ax=axes[i], square=True, cbar=color_bar_bool, cbar_ax=axes[-1], xticklabels=False, yticklabels=False, cbar_kws={"shrink":0.2})
-    axes[i].set_title(name+n_model)
-    axes[i].set_aspect('equal', adjustable='box')
-    previous_l = 0
-    for l in proteins_lengths:
-        rect = patches.Rectangle((previous_l, 0), l, -40, linewidth=1, edgecolor='r', facecolor='none', clip_on=False)
-        axes[i].add_patch(rect)
-        previous_l += l
-    n_model = str(int(n_model)+1)  
-
-axes[-1].set_aspect(1.5)
-
-plt.tight_layout()
-
-# for model in get_full_data(directory_path):
-#     pae_matrix = np.array(model['pae'])
-#     sns.heatmap(pae_matrix, cmap='viridis')
-#     ax = plt.gca()
-#     rect = patches.Rectangle((0, 0), 1000, -40, linewidth=1, edgecolor='r', facecolor='none', clip_on=False)
-#     ax.add_patch(rect)
-#     plt.title(name+n_model)
-#     plt.savefig(name+n_model+'.png')
-#     plt.clf()
-#     n_model = str(int(n_model)+1)  
+def plot_pae(data):
+    name = data[2]+" model "
+    proteins_lengths = data[1]
+    num_models = len(data[0])
+    total_length = sum(proteins_lengths)
+    n_model = "0"
+    fig, axes = plt.subplots(1, num_models, figsize=(8*num_models, 6))
+    for i, model in enumerate(data[0]):
+        pae_matrix = np.array(model['pae'])
+        sns.heatmap(pae_matrix, cmap='viridis', ax=axes[i], square=True, cbar=True, xticklabels=False, yticklabels=False, cbar_kws={"shrink":0.2})
+        axes[i].set_title(name+n_model, y=1.05)
+        axes[i].set_aspect('equal', adjustable='box')
+        previous_l = 0
+        palette = plt.cm.get_cmap("viridis")
+        palette = [palette(a / (len(proteins_lengths) - 1)) for a in range(len(proteins_lengths))] # Adapting to discrete palette
+        chain_ids = list(string.ascii_uppercase[:len(proteins_lengths)])
+        c = 0
+        for l in proteins_lengths:
+            rect_x = patches.Rectangle((previous_l, 0), l, -0.03*total_length, linewidth=1, edgecolor='none', facecolor=palette[c], clip_on=False)
+            rect_y = patches.Rectangle((total_length, previous_l), 0.03*total_length, l, linewidth=1, edgecolor='none',facecolor=palette[c], clip_on=False)
+            axes[i].add_patch(rect_x)
+            axes[i].add_patch(rect_y)
+            axes[i].text(previous_l + l / 2, -0.015*total_length, chain_ids[c], ha='center', va='center', color='black')
+            axes[i].text(1.015*total_length, previous_l + l / 2, chain_ids[c], ha='center', va='center', color='black')
+            previous_l += l
+            c += 1
+        n_model = str(int(n_model)+1)
+    plt.savefig(data[2]+"_pae.png", dpi=300.0)
 
 
+data = get_full_data(directory_path)
+plot_pae(data)
 
-# # Show the plot
-plt.show()
